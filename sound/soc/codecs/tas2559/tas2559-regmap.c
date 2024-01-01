ba925892a9125e589d8d87ee1478492bb4dddc56
JoysKo <joys.ko75@gmail.com>
2024-01-01 08:17:30 +0300

    sound: tas2559: do not print IRQ STATUS

--- sound/soc/codecs/tas2559/tas2559-regmap.c
+++ sound/soc/codecs/tas2559/tas2559-regmap.c
@@ -694,8 +694,6 @@ static void irq_work_routine(struct work_struct *work)
 
 			goto program;
 		} else {
-			dev_dbg(pTAS2559->dev, "IRQ status DevA: 0x%x, 0x%x\n",
-				nDevLInt1Status, nDevLInt2Status);
 			nCounter = 2;
 
 			while (nCounter > 0) {
@@ -795,8 +793,6 @@ static void irq_work_routine(struct work_struct *work)
 
 			goto program;
 		} else {
-			dev_dbg(pTAS2559->dev, "IRQ status DevB: 0x%x, 0x%x\n",
-				nDevRInt1Status, nDevRInt2Status);
 			nCounter = 2;
 
 			while (nCounter > 0) {
